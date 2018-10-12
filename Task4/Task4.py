3b0c8a2d4ed3fa9c88ac6e7e13bbf9e0b6be41ac
gualapuromoises <43993933+gualapuromoises@users.noreply.github.com>
2018-10-12 15:54:41 +0100

    Update Task4.py

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -1,11 +1,18 @@
-open file Task 3 ('dummy.pepmasses')
-read mass column
-separate results by enzyme
-define mass column ranges (user entry)
-count the number of events on each range, for each enzyme
-calculate mean values of mass on each range/bin for each enzyme
-calculate instruments accuracy (error) for each enzyme
-Identify patterns with Cys or Met for each enzyme
+Open file Task 3 ('dummy.pepmasses')
+Read mass column
+Separate results by enzyme
+Define mass column ranges (user entry)
+  Define your range, input (entry min value)
+  Define your range, input (entry max value)
+    or 
+  Define number of bins, input(how many bins want to visualice in hystogram)
+Count the number of peptides on each range, for each enzyme
+  Total of peptides
+  Subtotal of peptides with ions (ionA, ionB, ...)
+Calculate mean values of mass on each range/bin for each enzyme
+  Mean for all peptides by range
+  Mean of peptides by ions type (ionA, ionB, ...)
+Calculate instruments accuracy (error) for each enzyme
 Count peptides with Cys or Met
-transform mass values in Da to ppm
+Transform mass values from Da to ppm (conversion formula)
 Report CSV
