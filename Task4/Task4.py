09dbe2df78debdbd8af43554709cdc508254ea92
gualapuromoises <43993933+gualapuromoises@users.noreply.github.com>
2018-10-12 15:33:58 +0100

    Update Task4.py

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -1,1 +1,10 @@
-
+open ('dummy.pepmasses')
+read mass column
+separate results by enzyme
+define mass column ranges (user entry)
+count the number of events on each range, for each enzyme
+calculate mean values of mass on each range/bin for each enzyme
+calculate instruments accuracy (error) for each enzyme
+Identify patterns with Cys or Met for each enzyme
+Count peptides with Cys or Met
+Report CSV
