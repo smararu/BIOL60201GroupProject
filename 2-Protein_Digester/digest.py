e5cd5b4c79d88e22345f61725b584a99df852289
Abigail Wood <acjwd3@gmail.com>
2018-10-23 13:07:02 +0100

    Changed digest.py to take argument enzyme from command line

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -6,13 +6,13 @@ import re
 
 #To-do: use argparse module?
 sys_argv1 = sys.argv[1]
-sys_argv2 = sys.argv[2]
+enzyme = sys.argv[2]
 
 def read_proteins(sys_argv1): #processing task 1: process lines in .fasta file into a list of 2-tuples.
     proteins= []
     protein_name = ''
     sequence = ''
-    for line in open(sys.argv[1]):
+    for line in open(sys_argv1):
         if line.startswith('>'):
             sequence =''
             protein_name = line[1:-1] #remove newline character at end
@@ -28,12 +28,13 @@ print(proteins) #checks that the list of 2-tuples looks correct!
 #Create a function (e.g. digest) that can operate on my list of 2-tuples.
 def digest(proteins, enzyme): #defining a function called digest which takes arguments proteins and enzyme
     peptide_num = 1
-        if enzyme = trypsin
-        if enzyme =
-        if enzyme =
-        if enzyme =
+    if enzyme == 't':
+        print('you chose trypsin!')
+    else:
+        print('not implemented yet!')
+    return
 
-digest(proteins, enzyme)
+peptides = digest(proteins, enzyme)
 
 #my sequences start with > and end in *
 #if trypsin, identify first peptide at Lysine (K) or Arginine (R), and if the next amino acid is not Proline (P)
