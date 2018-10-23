577b66b1693414d9e0d8f96084e11f635489382f
Abigail Wood <acjwd3@gmail.com>
2018-10-23 16:36:33 +0100

    digest.py

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -14,29 +14,46 @@ def read_proteins(sys_argv1): #processing task 1: process lines in .fasta file i
     sequence = ''
     for line in open(sys_argv1):
         if line.startswith('>'):
-            sequence =''
+            if sequence !='':
+                proteins.append((protein_name,sequence))
+                sequence =''
             protein_name = line[1:-1] #remove newline character at end
         else:
-            line = line.strip()
-            sequence = line #append my sequence line to sequence
-            proteins.append((protein_name,sequence))
+            line = line.strip('\n')
+            sequence += line #append my sequence line to sequence
+    proteins.append((protein_name,sequence)) #p-to-do: could change to handle malformed files here
     return proteins
 
 proteins = read_proteins(sys_argv1)
-print(proteins) #checks that the list of 2-tuples looks correct!
+print(proteins)
 
 #Create a function (e.g. digest) that can operate on my list of 2-tuples.
 def digest(proteins, enzyme): #defining a function called digest which takes arguments proteins and enzyme
+    peptides=[]
     peptide_num = 1
+    i = 0
     if enzyme == 't':
-        print('you chose trypsin!')
-    else:
-        print('not implemented yet!')
+        print('you chose Trypsin!')
+        string =(proteins[i][1])
+        print(string)
+        i+= 1
+    if enzyme == 'l':
+        print('you chose Endoproteinase Lys-C!')
+        string =(proteins[i][1])
+    if enzyme == 'a':
+        print('you chose Endoproteinase Arg-C!')
+        string =(proteins[i][1])
+    if enzyme == 'e':
+        print('you chose V8 proteinase (Glu-C)!')
+        string =(proteins[i][1])
     return
 
 peptides = digest(proteins, enzyme)
 
-#my sequences start with > and end in *
+#take first tuple in proteins
+#scan along j (sequence), until a certain point, then split j!
+
+#my sequences end in *
 #if trypsin, identify first peptide at Lysine (K) or Arginine (R), and if the next amino acid is not Proline (P)
 #save sequence up to that point as protein 1 peptide 1
 #take remaining sequence and repeat process
