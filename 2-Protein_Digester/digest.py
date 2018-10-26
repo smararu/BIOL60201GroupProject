9f00c9d9be71c3dff0c72308c67aa72e8b706e63
Abigail Wood <acjwd3@gmail.com>
2018-10-26 18:57:27 +0100

    Changed my digest function to work with correct regular expressions for main task

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -29,54 +29,36 @@ proteins = read_proteins(sys_argv1)
 
 #create a dictionary consisting of key = enzyme code, and value = cleavage pattern
 recog_seq = {
-            't' : re.compile('[K|R]'),
-            'l' : re.compile('K'),
-            'a' : re.compile('R'),
-            'e' : re.compile('E')
+            't' : re.compile('([KR])(?!P)'),
+            'l' : re.compile('(K)(?!P)'),
+            'a' : re.compile('(R)(?!P)'),
+            'e' : re.compile('(E)(?!P)')
     }
 
 def digest(proteins, enzyme):
-    peptides=[]
+    full_peptides = []
+    peptides = []
     peptide_num = 0
-    i = 0
-    cleavage_start = recog_seq[enzyme]
+    pattern = recog_seq[enzyme]
     for name, sequence in proteins:
-        seq_peptides=re.split(cleavage_start, sequence)
-        for peptide in seq_peptides:
-            peptide_num+=1 #seq_peptides.len gives length
-            peptides.append({'name': name, 'peptide_num': peptide_num, 'peptide': peptide})
-            #print(f"> {proteins[i][0]} peptide {peptide_num} {enzyme}") #add dict to peptides list
-            i+= 1
+        peptides_unpaired = re.split(pattern, sequence)
+        peptide = zip(peptides_unpaired[::2], peptides_unpaired[1::2]) #run zip on seq_peptides to put pairs together.
+        for i, j in peptide:
+            full_peptide = i + j
+            full_peptides.append(full_peptide)
+            for peptide in full_peptides:
+                peptide_num += 1 #seq_peptides.len gives length
+                peptides.append({'name': name, 'peptide_num': peptide_num, 'peptide': peptide})
     return peptides
 
-peptides = digest(proteins, enzyme)
-#It's a list of dictionaries!!!!
+peptides = digest(proteins, enzyme) #It's a list of dictionaries!!!!
 
-#for loop to transfer list of dictionaries into .fasta format
 output=open('output.fasta','w')
-output2=open('output2.fasta','w')
-for peptide in peptides:
-    print ('>%s\t%d\t%s\n%s' % (peptide['name'], peptide['peptide_num'], enzyme, peptide['peptide']), file=output)
-    print(f">{peptide['name']}\t{peptide['peptide_num']}\t{enzyme}\n{peptide['peptide']}", file =output2)
+for peptide in peptides: #for loop to transfer list of dictionaries into .fasta format
+    print(f">{peptide['name']}\t{peptide['peptide_num']}\t'missed=0'\t{enzyme}\n{peptide['peptide']}", file =output)
 
-#print(f"> {output_name} peptide {output_peptide_num} {output_peptide}")
-
-
-#f"> {'name'} peptide {'peptide_num'} {'peptide'}")
-
-#output_name = [d['name'] for d in peptides]
-#output_peptide_num = [d['peptide_num'] for d in peptides]
-#output_peptide = [d['peptide'] for d in peptides]
-
-
-#my sequences end in *
-#if trypsin, identify first peptide at Lysine (K) or Arginine (R), and if the next amino acid is not Proline (P)
-#save sequence up to that point as protein 1 peptide 1
-#take remaining sequence and repeat process
-#when all of protein 1 has been fragmented, move to protein 2 and repeat process
-#when the list is finished, output all fragments to a file in fasta format (see Output format below)
-
-#bonus task 1 (cleavages): ensure Charles receives information in the form:
+#bonus task 1 (cleavages):
+#ensure Charles receives information in the form:
     #>protein_name peptide_num missed=int enzyme
     #'SEQUENCESTRING'
 
