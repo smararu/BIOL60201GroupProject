a2fde105f35f8a2d6ef7a0d60e86656f19ead5eb
Abigail Wood <acjwd3@gmail.com>
2018-10-26 15:37:35 +0100

    Changed digest.py to print my peptides in .fasta format

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -18,6 +18,7 @@ def read_proteins(sys_argv1): #processing task 1: process lines in .fasta file i
                 proteins.append((protein_name,sequence))
                 sequence =''
             protein_name = line[1:-1] #remove newline character at end
+            #replace any whitespace in protein_name with _
         else:
             line = line.rstrip('\n')
             sequence += line #append my sequence line to sequence
@@ -28,10 +29,10 @@ proteins = read_proteins(sys_argv1)
 
 #create a dictionary consisting of key = enzyme code, and value = cleavage pattern
 recog_seq = {
-            't' : re.compile('K|R'),
-            'l' : 'K',
-            'a' : 'R',
-            'e' : 'E'
+            't' : re.compile('[K|R]'),
+            'l' : re.compile('K'),
+            'a' : re.compile('R'),
+            'e' : re.compile('E')
     }
 
 def digest(proteins, enzyme):
@@ -39,21 +40,34 @@ def digest(proteins, enzyme):
     peptide_num = 0
     i = 0
     cleavage_start = recog_seq[enzyme]
-    print(cleavage_start)
     for name, sequence in proteins:
-        #if cleavage_start in sequence:
-            string = re.split(cleavage_start, sequence)
-            peptide_num+=1
-            print(f"> {proteins[i][0]} peptide {peptide_num} {enzyme}")
-            print('string', string)
+        seq_peptides=re.split(cleavage_start, sequence)
+        for peptide in seq_peptides:
+            peptide_num+=1 #seq_peptides.len gives length
+            peptides.append({'name': name, 'peptide_num': peptide_num, 'peptide': peptide})
+            #print(f"> {proteins[i][0]} peptide {peptide_num} {enzyme}") #add dict to peptides list
             i+= 1
-        #else: #something may be wrong with this
-            #string = sequence
-            #print(f"> {proteins[i][0]} peptide {peptide_num} {enzyme}")
-            #print('string', string)
-    return
+    return peptides
 
 peptides = digest(proteins, enzyme)
+#It's a list of dictionaries!!!!
+
+#for loop to transfer list of dictionaries into .fasta format
+output=open('output.fasta','w')
+output2=open('output2.fasta','w')
+for peptide in peptides:
+    print ('>%s\t%d\t%s\n%s' % (peptide['name'], peptide['peptide_num'], enzyme, peptide['peptide']), file=output)
+    print(f">{peptide['name']}\t{peptide['peptide_num']}\t{enzyme}\n{peptide['peptide']}", file =output2)
+
+#print(f"> {output_name} peptide {output_peptide_num} {output_peptide}")
+
+
+#f"> {'name'} peptide {'peptide_num'} {'peptide'}")
+
+#output_name = [d['name'] for d in peptides]
+#output_peptide_num = [d['peptide_num'] for d in peptides]
+#output_peptide = [d['peptide'] for d in peptides]
+
 
 #my sequences end in *
 #if trypsin, identify first peptide at Lysine (K) or Arginine (R), and if the next amino acid is not Proline (P)
@@ -63,5 +77,8 @@ peptides = digest(proteins, enzyme)
 #when the list is finished, output all fragments to a file in fasta format (see Output format below)
 
 #bonus task 1 (cleavages): ensure Charles receives information in the form:
-    #>protein_name peptide_num missed=int 'N/I/C' enzyme
+    #>protein_name peptide_num missed=int enzyme
     #'SEQUENCESTRING'
+
+    #>ClAUD_F1_1 peptide 1 missed=2 enzyme t
+    #GJAIGOEGKDGLLDGD
