c838192138ab46d8789b08cb256a54f79aa91a7f
Abigail Wood <acjwd3@gmail.com>
2018-11-02 20:34:11 +0000

    Added 4 output files from digest for each enzyme using realDummy.peps, changed digest.py to include argparse.

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -1,18 +1,30 @@
-import sys
-import re
+#SATURDAY
+#FIX BUG FOUND - REPEATED OUTPUT.
+#to-do: implement shebang
+#to-do: implement missed cleavages
+#error trapping, warnings, reporting.
+#sensible variable names
 
-#I want my command line input to look like:
-    #python digest.py -enzyme foo.fasta
+#!/usr/bin/env python 3
+#
+import re, argparse
 
-#To-do: use argparse module?
-sys_argv1 = sys.argv[1]
-enzyme = sys.argv[2]
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--filename', type=str, default='dummy.fasta',
+    help='the filename of .fasta file containing protein sequence(s)')
+parser.add_argument('-e', '--enzyme',
+    help='the name of an enzyme [t,l,a,e]', type=str, default='t')
+parser.add_argument('-m','--missed', type=int,
+    help='an integer value for number of missed cleavages[0-3]', default=0)
+parser.add_argument('-o','--output', type=str,
+    help='the output name of the file', default='output.fasta')
+args=parser.parse_args()
 
-def read_proteins(sys_argv1): #processing task 1: process lines in .fasta file into a list of 2-tuples.
+def read_proteins(input): #processing task 1: process lines in .fasta file into a list of 2-tuples.
     proteins= []
     protein_name = ''
     sequence = ''
-    for line in open(sys_argv1):
+    for line in open(args.filename):
         if line.startswith('>'):
             if sequence:
                 proteins.append((protein_name,sequence))
@@ -24,7 +36,7 @@ def read_proteins(sys_argv1): #processing task 1: process lines in .fasta file i
     proteins.append((protein_name,sequence)) #p-to-do: could change to handle malformed files here
     return proteins
 
-proteins = read_proteins(sys_argv1)
+proteins = read_proteins(args.filename)
 
 #create a dictionary consisting of key = enzyme code, and value = cleavage pattern
 recog_seq = {
@@ -36,7 +48,7 @@ recog_seq = {
 
 def digest(proteins, enzyme):
     peptides = []
-    pattern = recog_seq[enzyme]
+    pattern = recog_seq[args.enzyme]
     for name, sequence in proteins:
         full_peptides=[]
         peptide_num = 0
@@ -52,16 +64,28 @@ def digest(proteins, enzyme):
             peptides.append({'name': name, 'peptide_num': peptide_num, 'peptide': peptide})
     return peptides
 
-peptides = digest(proteins, enzyme) #It's a list of dictionaries!!!!
+peptides = digest(proteins, args.enzyme) #It's a list of dictionaries!!!!
 
-output=open('output.fasta','w')
+#def missed(proteins, missed):
+#    peptides_missed = []
+#    for peptide in peptides:
+#        peptides_missed.append(peptides[1], peptides[2], peptides[1]+peptides[2])
+#    return peptides_missed
+
+#peptides_missed=missed(proteins,args.missed)
+#print(peptides_missed)
+
+output=open(f'{args.output}','w')
 for peptide in peptides: #for loop to transfer list of dictionaries into .fasta format
-    print(f">{peptide['name']}\t{peptide['peptide_num']}\t'missed=0'\t{enzyme}\n{peptide['peptide']}", file =output)
+    print(f">{peptide['name']}\t{peptide['peptide_num']}\t'missed={args.missed}'\t{args.enzyme}\n{peptide['peptide']}", file =output)
 
-#bonus task 1 (cleavages):
+#bonus task 1 (missed cleavages):
+#list for 1 missed cleavage will look like:
+#peptide 1 + peptide 2 + peptide 3 (1+2) + peptide 4 + peptide 5 (2+4) + peptide 6 + peptide 7 (4+6) + peptide 8 + peptide 9 (6+8)
+
+#list for 2 missed cleavages will look like:
+#peptide 1 + peptide 2 + peptide 3 (1+2) + peptide 4 + peptide 5 (3+4) + peptide 6 + peptide 7 (2)
+
 #ensure Charles receives information in the form:
-    #>protein_name peptide_num missed=int enzyme
-    #'SEQUENCESTRING'
-
     #>ClAUD_F1_1 peptide 1 missed=2 enzyme t
     #GJAIGOEGKDGLLDGD
