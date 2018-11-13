8995abb3b20994160b2a70888c629fa79bea54e8
Abigail Wood <acjwd3@gmail.com>
2018-11-13 18:09:15 +0000

    Changed digest.py to improve main function

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -14,14 +14,14 @@ def parse_args():
     return args
 
 def read_proteins(file_input): #processes lines in .fasta file into a list of 2-tuples.
-    proteins= []
+    proteins = []
     protein_name = ''
     sequence = ''
     for line_num, line in enumerate(file_input, 1):
         if line.startswith('>'): #header lines in a .fasta file will always begin >
             if sequence:
                 proteins.append((protein_name,sequence))
-                sequence ='' #sequence is reset to an empty string for each new protein
+                sequence = '' #sequence is reset to an empty string for each new protein
             protein_name = line[1:-1] #removes first > and unwanted newline character at end of header line
         else: #any line not beginning > will be sequence in a .fasta file.
             line = line.rstrip('\n*')
@@ -54,24 +54,23 @@ def digest(sequence, enzyme):
     return peptides
 
 def missed(peptides, missed):
-    peptides_missed = []
+    full_peptides = []
     for n in range(1, missed + 2):
         for i in range(len(peptides) - n + 1):
             peptides_to_add = peptides[i:i+n]
-            peptides_missed.append(''.join(peptides_to_add))
-    return peptides_missed
+            full_peptides.append(''.join(peptides_to_add))
+    return full_peptides
 
 def main():
-    args=parse_args()
-    output=open(f'{args.output}','w')
-    proteins = read_proteins(args.file_input)
-    for name, sequence in proteins:
-        peptides = digest(sequence, args.enzyme) #It's a list of lists!!!!
-        peptides_missed=missed(peptides, args.missed)
-        peptide_num=0
-        for peptide in peptides_missed: #for loop to transfer list of dictionaries into .fasta format
-            peptide_num+=1
-            print(f">{name}\t{peptide_num}\tmissed={args.missed}\t{args.enzyme}\n{peptide}", file =output)
+    args = parse_args()
+    output = args.output
+    for protein_name, sequence in read_proteins(args.file_input):
+        peptides = digest(sequence, args.enzyme)
+        full_peptides = missed(peptides, args.missed)
+        peptide_num = 0
+        for peptide in full_peptides: #for loop to transfer list of dictionaries into .fasta format
+            peptide_num += 1
+            print(f">{protein_name}\t{peptide_num}\tmissed={args.missed}\t{args.enzyme}\n{peptide}", file =output)
 
 if __name__ == '__main__':
     main()
