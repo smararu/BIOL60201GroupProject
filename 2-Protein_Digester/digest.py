ac9305345fb663fe1d93178132310e152131fdd9
Abigail Wood <acjwd3@gmail.com>
2018-11-13 19:59:09 +0000

    Changed digest.py argparse and renamed some variables

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -2,28 +2,33 @@ import re, argparse, sys
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file_input', type=argparse.FileType('r'), default=sys.stdin,
+    parser.add_argument('-f', '--file_input', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
         help='the filename of .fasta file containing protein sequence(s) (defaults to standard input).')
-    parser.add_argument('-e', '--enzyme',
-        help='the name of an enzyme [t,l,a,e] (defaults to t).', type=str, default='t')
-    parser.add_argument('-m','--missed', type=int,
-        help='an integer value for number of missed cleavages[0-n] (defaults to 0).', default=0)
-    parser.add_argument('-o','--output', type=argparse.FileType('w'),
-        help='the output name of the file (defaults to standard output).', default=sys.stdout)
+    parser.add_argument('-e', '--enzyme', type=str, default='t',
+        help='the name of an enzyme [t,l,a,e] (defaults to t).')
+    parser.add_argument('-m','--missed', type=int, default=0,
+        help='an integer value for number of missed cleavages[0-n] (defaults to 0).')
+    parser.add_argument('-o','--output', type=argparse.FileType('w'), default=sys.stdout,
+        help='the output name of the file (defaults to standard output).')
     args=parser.parse_args()
     return args
 
-def read_proteins(file_input): #processes lines in .fasta file into a list of 2-tuples.
+#processes lines in .fasta file into a list of 2-tuples.
+def read_proteins(file_input):
     proteins = []
     protein_name = ''
     sequence = ''
     for line_num, line in enumerate(file_input, 1):
-        if line.startswith('>'): #header lines in a .fasta file will always begin >
+        #header lines in a .fasta file will always begin >, so we can use this to identify protein name.
+        if line.startswith('>'):
             if sequence:
                 proteins.append((protein_name,sequence))
-                sequence = '' #sequence is reset to an empty string for each new protein
-            protein_name = line[1:-1] #removes first > and unwanted newline character at end of header line
-        else: #any line not beginning > will be sequence in a .fasta file.
+                #sequence is reset to an empty string for each new protein
+                sequence = ''
+            #removes first > and unwanted newline character at end of header line
+            protein_name = line[1:-1]
+        #any line not beginning > will be sequence in a .fasta file.
+        else:
             line = line.rstrip('\n*')
             if re.search('[BOUJZ]', line):
                 print(f'Warning: unusual amino acid (B,O,U,J,Z) found in {protein_name} on line {line_num}', file=sys.stderr)
@@ -45,15 +50,15 @@ def digest(sequence, enzyme):
     peptides = []
     pattern = recog_seq[enzyme]
     peptides_unpaired = re.split(pattern, sequence)
-    peptide = zip(peptides_unpaired[::2], peptides_unpaired[1::2]) #run zip on seq_peptides to put pairs together.
-    for i, j in peptide:
-        full_peptide = i + j
-        peptides.append(full_peptide)
-    if peptides_unpaired[-1]: #if the cleavage site is not at the end of the protein, zip will leave off the final peptide.
+    # Run zip on seq_peptides to put pairs together.
+    peptides_paired = zip(peptides_unpaired[::2], peptides_unpaired[1::2])
+    peptides = [p+c for p,c in peptides_paired]
+    # If the cleavage site is not at the end of the protein, zip will leave off the final peptide, so we add it.
+    if peptides_unpaired[-1]:
         peptides.append(peptides_unpaired[-1])
     return peptides
 
-def missed(peptides, missed):
+def combine(peptides, missed):
     full_peptides = []
     for n in range(1, missed + 2):
         for i in range(len(peptides) - n + 1):
@@ -65,14 +70,13 @@ def output(output, protein_name, full_peptides):
     peptide_num = 0
     for peptide in full_peptides:
         peptide_num += 1
-        print(f">{protein_name}\t{peptide_num}\tmissed={args.missed}\t{args.enzyme}\n{peptide}", file = output_peptides)
-    return output_peptides
+        print(f">{protein_name} {peptide_num} missed={args.missed} {args.enzyme}\n{peptide}", file = output)
 
 def main():
     args = parse_args()
     for protein_name, sequence in read_proteins(args.file_input):
         peptides = digest(sequence, args.enzyme)
-        full_peptides = missed(peptides, args.missed)
+        full_peptides = combine(peptides, args.missed)
         output_peptides = output(args.output, protein_name, full_peptides)
 
 if __name__ == '__main__':
