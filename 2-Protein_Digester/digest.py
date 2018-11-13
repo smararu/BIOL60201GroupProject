360bb7d14b38841d3be271519256819a583e8ce3
Abigail Wood <acjwd3@gmail.com>
2018-11-13 16:55:03 +0000

    Changed digest.py to put main function in right place.

--- 2-Protein_Digester/digest.py
+++ 2-Protein_Digester/digest.py
@@ -1,17 +1,5 @@
 import re, argparse, sys
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filename', type=str, default='sys.stdin',
-        help='the filename of .fasta file containing protein sequence(s) (defaults to standard input)')
-    parser.add_argument('-e', '--enzyme',
-        help='the name of an enzyme [t,l,a,e] (defaults to t)', type=str, default='t')
-    parser.add_argument('-m','--missed', type=int,
-        help='an integer value for number of missed cleavages[0-n] (defaults to 0)', default=0)
-    parser.add_argument('-o','--output', type=str,
-        help='the output name of the file (defaults to standard output)', default='sys.stdout')
-    args=parser.parse_args()
-
 def read_proteins(filename): #processes lines in .fasta file into a list of 2-tuples.
     proteins= []
     protein_name = ''
@@ -60,15 +48,27 @@ def missed(peptides, missed):
             peptides_missed.append(''.join(peptides_to_add))
     return peptides_missed
 
-output=open(f'{args.output}','w')
-proteins = read_proteins(args.filename)
-for name, sequence in proteins:
-    peptides = digest(sequence, args.enzyme) #It's a list of lists!!!!
-    peptides_missed=missed(peptides, args.missed)
-    peptide_num=0
-    for peptide in peptides_missed: #for loop to transfer list of dictionaries into .fasta format
-        peptide_num+=1
-        print(f">{name}\t{peptide_num}\tmissed={args.missed}\t{args.enzyme}\n{peptide}", file =output)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', type=str, default='sys.stdin',
+        help='the filename of .fasta file containing protein sequence(s) (defaults to standard input)')
+    parser.add_argument('-e', '--enzyme',
+        help='the name of an enzyme [t,l,a,e] (defaults to t)', type=str, default='t')
+    parser.add_argument('-m','--missed', type=int,
+        help='an integer value for number of missed cleavages[0-n] (defaults to 0)', default=0)
+    parser.add_argument('-o','--output', type=str,
+        help='the output name of the file (defaults to standard output)', default='sys.stdout')
+    args=parser.parse_args()
+
+    output=open(f'{args.output}','w')
+    proteins = read_proteins(args.filename)
+    for name, sequence in proteins:
+        peptides = digest(sequence, args.enzyme) #It's a list of lists!!!!
+        peptides_missed=missed(peptides, args.missed)
+        peptide_num=0
+        for peptide in peptides_missed: #for loop to transfer list of dictionaries into .fasta format
+            peptide_num+=1
+            print(f">{name}\t{peptide_num}\tmissed={args.missed}\t{args.enzyme}\n{peptide}", file =output)
 
 if __name__ == '__main__':
     main()
